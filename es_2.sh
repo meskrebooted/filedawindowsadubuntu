cec0638dacd688985024860ab8ad9c5335d7052b
mesk <187385403+meskrebooted@users.noreply.github.com>
2025-12-05 13:11:32 +0100

    Add files via upload

---
+++ es_2.sh
@@ -0,0 +1,15 @@
+mkdir jolly
+cd jolly
+touch andrea.txt
+echo "andrea" > andrea.txt
+touch giuseppe.doc
+echo "giuseppe" > giuseppe.doc
+touch alessandro.txt
+echo "alessandro" > alessandro.txt
+touch giovanni.txt
+echo "giovanni" > giovanni.txt
+ls ??e*.txt
+mkdir jolly_2
+cp ??????.txt jolly_2
+rm *.doc
+cat *.txt > risultati.txt
