5718bd549b77b8742a4fe921d9440307db050799
mesk <187385403+meskrebooted@users.noreply.github.com>
2025-12-05 13:25:30 +0100

    Refactor es_2.sh to create and manage files

--- es_2.sh
+++ es_2.sh
@@ -1,15 +1,11 @@
-mkdir jolly
-cd jolly
-touch andrea.txt
-echo "andrea" > andrea.txt
-touch giuseppe.doc
-echo "giuseppe" > giuseppe.doc
-touch alessandro.txt
-echo "alessandro" > alessandro.txt
-touch giovanni.txt
-echo "giovanni" > giovanni.txt
-ls ??e*.txt
-mkdir jolly_2
-cp ??????.txt jolly_2
-rm *.doc
-cat *.txt > risultati.txt
+#!/bin/bash
+mkdir -p ORIGINE/MIEI_FILE
+echo "contenuto file 1" > ORIGINE/1.txt
+echo "contenuto file 2" > ORIGINE/2.doc
+echo "contenuto file a" > ORIGINE/a.txt
+echo "contenuto file x" > ORIGINE/x.dll
+chmod 444 ORIGINE/2.doc
+mv ORIGINE/1.txt ORIGINE/finale.txt
+cat ORIGINE/finale.txt
+mv ORIGINE/* ORIGINE/MIEI_FILE/
+ls ORIGINE/MIEI_FILE
